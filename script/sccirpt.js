6b44a05d1e57b55f22b7e87349864fa20ef9458b
mdmasudrana13 <masudxyz13@gmail.com>
2026-02-24 17:22:51 +0600

    dailigation

--- script/sccirpt.js
+++ script/sccirpt.js
@@ -1,16 +1,16 @@
-let interviewList =[];
-let rejectedList =[];
-let total=document.getElementById('total')
-let interview=document.getElementById('Interview')
-let rejected =document.getElementById('Rejected')
+let interviewList = [];
+let rejectedList = [];
+let total = document.getElementById('total')
+let interview = document.getElementById('Interview')
+let rejected = document.getElementById('Rejected')
 
 const allcard = document.getElementById('allcards')
-const mainCont =document.querySelector('main')
+const mainCont = document.querySelector('main')
 
-function count(){
-    total.innerText=allcard.children.length
-    interview.innerText=interviewList.length
-    rejected.innerText=rejectedList.length
+function count() {
+   total.innerText = allcard.children.length
+   interview.innerText = interviewList.length
+   rejected.innerText = rejectedList.length
 
 }
 count()
@@ -20,35 +20,43 @@ count()
 // const rejectedFilterBtn =document.getElementById('Rejected-btn-id')
 
 
-const alllFilterBtn=document.getElementById('all-btn-id')
-const interviewFilterBtn=document.getElementById('Interview-btn-id')
-const rejectedFilterBtn =document.getElementById('Rejected-btn-id')
+const alllFilterBtn = document.getElementById('all-btn-id')
+const interviewFilterBtn = document.getElementById('Interview-btn-id')
+const rejectedFilterBtn = document.getElementById('Rejected-btn-id')
 
-function toggleStyle(id){
+function toggleStyle(id) {
 
    alllFilterBtn.classList.remove('bg-blue-500', 'text-white')
    interviewFilterBtn.classList.remove('bg-blue-500', 'text-white')
-   rejectedFilterBtn.classList.remove('bg-blue-500', 'text-white') 
+   rejectedFilterBtn.classList.remove('bg-blue-500', 'text-white')
 
    alllFilterBtn.classList.add('bg-white', 'text-gray-500')
    interviewFilterBtn.classList.add('bg-white', 'text-gray-500')
-   rejectedFilterBtn.classList.add('bg-white', 'text-gray-500') 
+   rejectedFilterBtn.classList.add('bg-white', 'text-gray-500')
 
 
- const selected =document.getElementById(id) 
- selected.classList.remove('bg-white', 'text-gray-500')
-  selected.classList.add('bg-black','text-white')
-  
-  
+   const selected = document.getElementById(id)
+   selected.classList.remove('bg-white', 'text-gray-500')
+   selected.classList.add('bg-black', 'text-white')
+
+
+}
+mainCont.addEventListener('click', function (event) {
+   const parenNodes = event.target.parentNode.parentNode;
+   const titaleName = parenNodes.querySelector('.titale').innerText;
+   const dissName = parenNodes.querySelector('.diss').innerText;
+   const detlsName = parenNodes.querySelector('.detls').innerText;
+   const statusName = parenNodes.querySelector('.status').innerText;
+   const moreName = parenNodes.querySelector('.more').innerText;
+   // // console.log('titaleName','dissName','etlsName','statusName','moreName');
+
+
+   const cardinfo = {
+      titaleName, dissName, detlsName, statusName, moreName
    }
-mainCont.addEventListener('click',function(event){
-   const parenNodes =event.target.parentNode.parentNode;
-   const titaleName=parenNodes.querySelector('.titale').innerText ;
-   
-   
 
-  
-    
-    
+   console.log(cardinfo);
+
+
 })
 
